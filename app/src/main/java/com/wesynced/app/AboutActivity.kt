29b581ddaeb2b8366fa28f14108fb2ec4ad4a8b8
wrongxide-cyber <id.manas.b@gmail.com>
2026-09-12 21:39:42 +0530

    Create AboutActivity.kt

---
+++ app/src/main/java/com/wesynced/app/AboutActivity.kt
@@ -0,0 +1,25 @@
+package com.wesynced.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wesynced.app.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbarAbout.setNavigationOnClickListener { finish() }
+
+        val versionName = try {
+            packageManager.getPackageInfo(packageName, 0).versionName
+        } catch (e: Exception) {
+            "1.0.0"
+        }
+        binding.tvAppVersion.text = getString(R.string.about_version_prefix, versionName)
+    }
+}
