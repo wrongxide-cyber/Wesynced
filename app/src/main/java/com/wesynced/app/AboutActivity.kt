c5df7b533d331cb61847d61595673a091e0975d0
wrongxide-cyber <id.manas.b@gmail.com>
2026-09-15 01:25:51 +0530

    Update AboutActivity.kt

--- app/src/main/java/com/wesynced/app/AboutActivity.kt
+++ app/src/main/java/com/wesynced/app/AboutActivity.kt
@@ -1,6 +1,7 @@
 package com.wesynced.app
 
 import android.os.Bundle
+import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
 import com.wesynced.app.databinding.ActivityAboutBinding
 
@@ -10,16 +11,51 @@ class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAboutBinding.inflate(layoutInflater)
+
+        binding =
+            ActivityAboutBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
-        binding.toolbarAbout.setNavigationOnClickListener { finish() }
+        applyStatusBarInset()
 
-        val versionName = try {
-            packageManager.getPackageInfo(packageName, 0).versionName
-        } catch (e: Exception) {
-            "1.0.0"
+        val versionName =
+            try {
+                packageManager
+                    .getPackageInfo(
+                        packageName,
+                        0
+                    )
+                    .versionName
+            } catch (e: Exception) {
+                "1.1.0"
+            }
+
+        binding.tvAppVersion.text =
+            getString(
+                R.string.about_version_prefix,
+                versionName
+            )
+    }
+
+    private fun applyStatusBarInset() {
+
+        val toolbar =
+            binding.toolbarAbout
+
+        toolbar.setOnApplyWindowInsetsListener { view, insets ->
+
+            val statusBarHeight =
+                insets.getInsets(
+                    WindowInsets.Type.statusBars()
+                ).top
+
+            view.translationY =
+                statusBarHeight.toFloat()
+
+            insets
         }
-        binding.tvAppVersion.text = getString(R.string.about_version_prefix, versionName)
+
+        toolbar.requestApplyInsets()
     }
 }
