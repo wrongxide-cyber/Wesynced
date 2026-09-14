783608d4786554dfe3b6daab44b71d3fee52a88b
wrongxide-cyber <id.manas.b@gmail.com>
2026-09-15 01:26:23 +0530

    Update HelpActivity.kt

--- app/src/main/java/com/wesynced/app/HelpActivity.kt
+++ app/src/main/java/com/wesynced/app/HelpActivity.kt
@@ -1,14 +1,43 @@
 package com.wesynced.app
 
 import android.os.Bundle
+import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
 import com.wesynced.app.databinding.ActivityHelpBinding
 
 class HelpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHelpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityHelpBinding.inflate(layoutInflater)
+
+        binding =
+            ActivityHelpBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
-        binding.toolbarHelp.setNavigationOnClickListener { finish() }
+
+        applyStatusBarInset()
+    }
+
+    private fun applyStatusBarInset() {
+
+        val toolbar =
+            binding.toolbarHelp
+
+        toolbar.setOnApplyWindowInsetsListener { view, insets ->
+
+            val statusBarHeight =
+                insets.getInsets(
+                    WindowInsets.Type.statusBars()
+                ).top
+
+            view.translationY =
+                statusBarHeight.toFloat()
+
+            insets
+        }
+
+        toolbar.requestApplyInsets()
     }
 }
