d5f2e87fbe30beffe4591a15785c4ffefa6f53cb
wrongxide-cyber <id.manas.b@gmail.com>
2026-09-15 02:16:44 +0530

    Update SettingsActivity.kt

--- app/src/main/java/com/wesynced/app/SettingsActivity.kt
+++ app/src/main/java/com/wesynced/app/SettingsActivity.kt
@@ -1,8 +1,6 @@
 package com.wesynced.app
 
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
 import com.wesynced.app.databinding.ActivitySettingsBinding
 
@@ -16,85 +14,29 @@ class SettingsActivity : AppCompatActivity() {
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        applyStatusBarInset()
+        binding.toolbarSettings.setNavigationOnClickListener { finish() }
+
         setupThemeToggle()
     }
 
-    private fun applyStatusBarInset() {
-        val toolbar = binding.toolbarSettings
-
-        toolbar.setOnApplyWindowInsetsListener { view, insets ->
-
-            val statusBarHeight =
-                insets.getInsets(
-                    WindowInsets.Type.statusBars()
-                ).top
-
-            view.translationY =
-                statusBarHeight.toFloat()
-
-            val params =
-                view.layoutParams
-
-            params.height =
-                resources.getDimensionPixelSize(
-                    com.google.android.material.R.dimen.mtrl_toolbar_default_height
-                )
-
-            view.layoutParams = params
-
-            insets
-        }
-
-        toolbar.requestApplyInsets()
-    }
-
     private fun setupThemeToggle() {
-
-        val currentMode =
-            AppSettings.getThemeMode(this)
-
-        val isDark =
-            currentMode == AppSettings.THEME_DARK
-
-        binding.switchDarkMode.isChecked =
-            isDark
-
+        val currentMode = AppSettings.getThemeMode(this)
+        val isDark = currentMode == AppSettings.THEME_DARK
+        binding.switchDarkMode.isChecked = isDark
         updateThemeStatusLabel(isDark)
 
-        binding.switchDarkMode
-            .setOnCheckedChangeListener { _, isChecked ->
-
-                val mode =
-                    if (isChecked) {
-                        AppSettings.THEME_DARK
-                    } else {
-                        AppSettings.THEME_LIGHT
-                    }
-
-                AppSettings.setThemeMode(
-                    this,
-                    mode
-                )
-
-                updateThemeStatusLabel(
-                    isChecked
-                )
-            }
+        binding.switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
+            val mode = if (isChecked) AppSettings.THEME_DARK else AppSettings.THEME_LIGHT
+            AppSettings.setThemeMode(this, mode)
+            updateThemeStatusLabel(isChecked)
+        }
     }
 
-    private fun updateThemeStatusLabel(
-        isDark: Boolean
-    ) {
-        binding.tvThemeStatus.text =
-            if (isDark) {
-                getString(
-                    R.string.settings_theme_dark
-                )
-            } else {
-                getString(
-                    R.string.settings_theme_light
-                )
-            }
+    private fun updateThemeStatusLabel(isDark: Boolean) {
+        binding.tvThemeStatus.text = if (isDark) {
+            getString(R.string.settings_theme_dark)
+        } else {
+            getString(R.string.settings_theme_light)
+        }
     }
 }
