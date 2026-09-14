1bbf14de1a7a14992169ed990aab9580f21a7219
wrongxide-cyber <id.manas.b@gmail.com>
2026-09-15 00:39:05 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -63,6 +63,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-implementation("com.google.firebase:firebase-messaging-ktx:24.1.1")
-implementation("androidx.viewpager2:viewpager2:1.1.0")
+
+    implementation("com.google.firebase:firebase-messaging-ktx:24.1.1")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
 }
