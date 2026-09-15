ea40023b9c37a8813116afb95c3768a29aaa770e
wrongxide-cyber <id.manas.b@gmail.com>
2026-09-16 02:39:05 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.wesynced.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.1.0"
+        versionCode = 3
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
