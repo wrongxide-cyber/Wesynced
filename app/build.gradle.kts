898ca6ce745820757f59dbc54b8e77d71f6c109e
wrongxide-cyber <id.manas.b@gmail.com>
2026-09-16 13:12:43 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,6 @@ dependencies {
     implementation("androidx.viewpager2:viewpager2:1.1.0")
 
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.airbnb.android:lottie:6.4.0")
+
 }
